b03cc301d38711b1f87c45d90c9ecf145e8215f0
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-04-04 23:52:33 -0700

    Update project samples to Meadow.F7 NuGet

--- Source/CharlieWing/Samples/CharlieWing_Sample/MeadowApp.cs
+++ Source/CharlieWing/Samples/CharlieWing_Sample/MeadowApp.cs
@@ -7,7 +7,7 @@ using Meadow.Foundation.Graphics;
 
 namespace FeatherWings.CharlieWing_Sample
 {
-    public class MeadowApp : App<F7FeatherV2, MeadowApp>
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
     {
         //<!—SNIP—>
 
--- Source/KeyboardWing/Samples/KeyboardWing_Sample/MeadowApp.cs
+++ Source/KeyboardWing/Samples/KeyboardWing_Sample/MeadowApp.cs
@@ -7,7 +7,7 @@ using Meadow.Hardware;
 
 namespace FeatherWings.KeyboardWing_Sample
 {
-    public class MeadowApp : App<F7FeatherV2, MeadowApp>
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
     {
         //<!—SNIP—>
 
