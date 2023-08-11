305ef33ef9a9762f5b5bf8cc1d5adcd15d3572f7
Adrian Stevens <adrian.stevens@gmail.com>
2023-08-10 20:28:30 -0700

    Add NeoPixel wing sample and driver

---
+++ Source/NeoPixelWing/Sample/NeoPixelWing_Sample/MeadowApp.cs
@@ -0,0 +1,42 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.FeatherWings;
+using Meadow.Foundation.Graphics;
+using System;
+using System.Threading.Tasks;
+
+namespace FeatherWings.NeoPixel_Sample
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        //<!=SNIP=>
+
+        NeoPixelWing neoWing;
+        MicroGraphics graphics;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initializing ...");
+
+            neoWing = new NeoPixelWing(Device.CreateSpiBus(), Device.Pins.D10);
+
+            graphics = new MicroGraphics(neoWing);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+         //   graphics.Clear();
+
+            graphics.DrawRectangle(0, 0, 8, 4, Color.Red.WithBrightness(0.1), false);
+
+            graphics.Show();
+
+            return Task.CompletedTask;
+        }
+
+        //<!=SNOP=>
+    }
+}
