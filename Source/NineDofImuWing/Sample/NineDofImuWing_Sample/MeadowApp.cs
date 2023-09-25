e0255d05f7fba682dd0ce5386604d8864acd5b97
Branden Gunn <engunneer@gmail.com>
2023-09-24 23:02:20 -0400

    better event integration

--- Source/NineDofImuWing/Sample/NineDofImuWing_Sample/MeadowApp.cs
+++ Source/NineDofImuWing/Sample/NineDofImuWing_Sample/MeadowApp.cs
@@ -16,7 +16,7 @@ namespace NineDofImuWing_Sample
 
         public override Task Initialize()
         {
-            Console.WriteLine("Initializing ...");
+            Resolver.Log.Info("Initializing ...");
 
             var i2cBus = Device.CreateI2cBus(I2cBusSpeed.FastPlus);
 
@@ -28,9 +28,10 @@ namespace NineDofImuWing_Sample
 
             nineDofImuWing.Subscribe(consumer);
 
-            // classical .NET events can also be used:
+            // classical .NET events can also be used for all sensors
             nineDofImuWing.Updated += HandleResult;
-
+            
+            // or for individual sensors
             nineDofImuWing.Acceleration3DUpdated += HandleResult;
             nineDofImuWing.AngularVelocity3DUpdated += HandleResult;
             nineDofImuWing.MagneticField3DUpdated += HandleResult;
@@ -40,40 +41,48 @@ namespace NineDofImuWing_Sample
             return Task.CompletedTask;
         }
 
-        bool FilterResult(
-            IChangeResult<(Acceleration3D? Acceleration3D, 
-            AngularVelocity3D? AngularVelocity3D, 
-            MagneticField3D? MagneticField3D)> result)
+        bool FilterResult(IChangeResult<(
+                Acceleration3D? Acceleration3D, 
+                AngularVelocity3D? AngularVelocity3D, 
+                MagneticField3D? MagneticField3D
+            )> result)
         {
             return result.New.Acceleration3D.Value.Z > new Acceleration(0.1, Acceleration.UnitType.Gravity);
         }
 
-        void HandleResult(object sender,
-            IChangeResult<(Acceleration3D? Acceleration3D,
-            AngularVelocity3D? AngularVelocity3D,
-            MagneticField3D? MagneticField3D)> result)
+        // Example Event Handler for all sensor results
+        void HandleResult(object sender, IChangeResult<(
+                Acceleration3D? Acceleration3D, 
+                AngularVelocity3D? AngularVelocity3D, 
+                MagneticField3D? MagneticField3D
+            )> result)
         {
             var accel = result.New.Acceleration3D.Value;
             var gyro = result.New.AngularVelocity3D.Value;
             var mag = result.New.MagneticField3D.Value;
 
-            Resolver.Log.Info($"AccelX={accel.X.Gravity:0.##}g, AccelY={accel.Y.Gravity:0.##}g, AccelZ={accel.Z.Gravity:0.##}g, GyroX={gyro.X.DegreesPerSecond:0.##}°/s, GyroY={gyro.Y.DegreesPerSecond:0.##}°/s, GyroZ={gyro.Z.DegreesPerSecond:0.##}°/s, MagX={mag.X.Gauss:0.##}gauss, MagY={mag.Y.Gauss:0.##}gauss, GyroZ={mag.Z.Gauss:0.##}gauss");
+            Resolver.Log.Info(
+                $"Accelerometer (g): X = {accel.X.Gravity:0.##}, Y = {accel.Y.Gravity:0.##}, Z = {accel.Z.Gravity:0.##}; " +
+                $"Gyroscope (°/s): X = {gyro.X.DegreesPerSecond:0.##}, Y = {gyro.Y.DegreesPerSecond:0.##}, Z = {gyro.Z.DegreesPerSecond:0.##}; " +
+                $"Magnetometer (gauss) X = {mag.X.Gauss:0.##}, Y = {mag.Y.Gauss:0.##}, Z = {mag.Z.Gauss:0.##}"
+                );
         }
 
+        // Example Event Handler for any individual sensors' result
         void HandleResult<UNIT>(object sender, IChangeResult<UNIT> result) 
         where UNIT : struct
         {
             if (result is IChangeResult<Acceleration3D> accel)
             {
-                Resolver.Log.Info($"AccelX={accel.New.X.Gravity:0.##}g, AccelY={accel.New.Y.Gravity:0.##}g, AccelZ={accel.New.Z.Gravity:0.##}g");
+                Resolver.Log.Info($"Accelerometer (g): X = {accel.New.X.Gravity:0.##}, Y = {accel.New.Y.Gravity:0.##}, Z = {accel.New.Z.Gravity:0.##}");
             }
             else if (result is IChangeResult<AngularVelocity3D> gyro)
             {
-                Resolver.Log.Info($"GyroX={gyro.New.X.DegreesPerSecond:0.##}°/s, GyroY={gyro.New.Y.DegreesPerSecond:0.##}°/s, GyroZ={gyro.New.Z.DegreesPerSecond:0.##}°/s");
+                Resolver.Log.Info($"Gyroscope (°/s): X = {gyro.New.X.DegreesPerSecond:0.##}, Y = {gyro.New.Y.DegreesPerSecond:0.##}, Z = {gyro.New.Z.DegreesPerSecond:0.##};");
             }
             else if (result is IChangeResult<MagneticField3D> mag)
             {
-                Resolver.Log.Info($"MagX={mag.New.X.Gauss:0.##}gauss, MagY={mag.New.Y.Gauss:0.##}gauss, MagZ={mag.New.Z.Gauss:0.##}gauss");
+                Resolver.Log.Info($"Magnetometer (gauss) X = {mag.New.X.Gauss:0.##}, Y = {mag.New.Y.Gauss:0.##}, Z = {mag.New.Z.Gauss:0.##}");
             }
         }
 
