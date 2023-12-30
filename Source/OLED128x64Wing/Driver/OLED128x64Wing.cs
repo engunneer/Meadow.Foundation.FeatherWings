2afb34b203d8ce0a090e6398d7f6274ca535d215
Branden Gunn <engunneer@gmail.com>
2023-12-29 22:38:22 -0500

    Add 128x64 OLED wing

---
+++ Source/OLED128x64Wing/Driver/OLED128x64Wing.cs
@@ -0,0 +1,68 @@
+﻿using Meadow.Foundation.Displays;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.FeatherWings
+{
+    /// <summary>
+    /// Represents Adafruit's 128 x 64 OLED Feather Wing
+    /// </summary>
+    public class OLED128x64Wing
+    {
+        /// <summary>
+        /// Returns SH1107 object
+        /// </summary>
+        public Sh1107 Display { get; protected set; }
+
+        /// <summary>
+        /// Returns button A
+        /// </summary>
+        public PushButtonBase ButtonA { get; protected set; }
+
+        /// <summary>
+        /// Returns button B
+        /// </summary>
+        public PushButtonBase ButtonB { get; protected set; }
+
+        /// <summary>
+        /// Returns button C
+        /// </summary>
+        public PushButtonBase ButtonC { get; protected set; }
+
+        /// <summary>
+        /// Creates a OLED128x64Wing driver. Buttons will be <see cref="PushButton"/> if capable of the interrupt, or <see cref="PollingPushButton"/> otherwise.
+        /// </summary>
+        /// <param name="i2cBus">The I2C bus connected to the wing</param>
+        /// <param name="pinA">The pin used for button A. Usually D11 for Feather format Meadow devices.</param>
+        /// <param name="pinB">The pin used for button B. Usually D10 for Feather format Meadow devices.</param>
+        /// <param name="pinC">The pin used for button C. Usually D09 for Feather format Meadow devices.</param>
+        public OLED128x64Wing(II2cBus i2cBus, IPin pinA, IPin pinB, IPin pinC)
+        {
+            Display = new Sh1107(i2cBus, (byte)Sh1107.Addresses.Address_0x3C, width: 64, height: 128, firstColumn: 32);
+
+            ButtonA = BestPossiblePushButton(pinA);
+            ButtonB = BestPossiblePushButton(pinB);
+            ButtonC = BestPossiblePushButton(pinC);
+        }
+
+        // TODO: A version of this method may want to be folded into Meadow.Foundation directly?
+        /// <summary>
+        /// Creates an interrupt-driven <see cref="PushButton"/> if possible, falling back to <see cref="PollingPushButton"/> if needed.
+        /// </summary>
+        /// <param name="pin">The pin connected to the button</param>
+        /// <returns><see cref="PushButton"/> or <see cref="PollingPushButton"/> depending on the capabilities of the <paramref name="pin"/></returns>
+        /// <remarks>This exists because the A, B, and C default pins on the Meadow F7 V1 and V2 differ in interrupt capabilities.</remarks>
+        private PushButtonBase BestPossiblePushButton(IPin pin)
+        {
+            if (pin.Supports<IDigitalChannelInfo>(info => info.InterruptCapable))
+            {
+                return new PushButton(pin.CreateDigitalInterruptPort(InterruptMode.EdgeBoth, ResistorMode.InternalPullUp));
+            }
+            else
+            {
+                return new PollingPushButton(pin) { LongClickedThreshold = TimeSpan.Zero };
+            }
+        }
+    }
+}
---
+++ Source/OLED128x64Wing/Driver/Readme.md
@@ -0,0 +1,56 @@
+# Meadow.Foundation.FeatherWings.OLED128x64Wing
+
+**AdaFruit OLED 128x64 monochrome display FeatherWing**
+
+The **OLED128x64** library is designed for the [Wilderness Labs](www.wildernesslabs.co) Meadow .NET IoT platform and is part of [Meadow.Foundation](https://developer.wildernesslabs.co/Meadow/Meadow.Foundation/)
+
+The **Meadow.Foundation** peripherals library is an open-source repository of drivers and libraries that streamline and simplify adding hardware to your C# .NET Meadow IoT application.
+
+For more information on developing for Meadow, visit [developer.wildernesslabs.co](http://developer.wildernesslabs.co/).
+
+To view all Wilderness Labs open-source projects, including samples, visit [github.com/wildernesslabs](https://github.com/wildernesslabs/).
+
+## Usage
+
+```csharp
+OLED128x64Wing oledWing;
+MicroGraphics graphics;
+
+public override Task Initialize()
+{
+    Resolver.Log.Info("Initializing...");
+    var i2cBus = Device.CreateI2cBus(I2cBusSpeed.FastPlus);
+
+    oledWing = new OLED128x64Wing(i2cBus, Device.Pins.D11, Device.Pins.D10, Device.Pins.D09);
+
+    graphics = new MicroGraphics(oledWing.Display);
+    // Use RotationType.Default for "native" screen orientation or RotationType._90Degrees for "wide" orientation.
+    graphics.Rotation = RotationType._90Degrees;
+    graphics.CurrentFont = new Font8x8();
+
+    oledWing.ButtonA.Clicked += (sender, e) => UpdateDisplay("A Clicked");
+    oledWing.ButtonB.Clicked += (sender, e) => UpdateDisplay("B Clicked");
+    oledWing.ButtonC.Clicked += (sender, e) => UpdateDisplay("C Clicked");
+
+    UpdateDisplay("Ready");
+    return Task.CompletedTask;
+}
+
+void UpdateDisplay(string message)
+{
+    graphics.Clear();
+    graphics.DrawText(x: 0, y: 8, message);
+    graphics.Show();
+}
+```
+
+## How to Contribute
+
+- **Found a bug?** [Report an issue](https://github.com/WildernessLabs/Meadow_Issues/issues)
+- Have a **feature idea or driver request?** [Open a new feature request](https://github.com/WildernessLabs/Meadow_Issues/issues)
+- Want to **contribute code?** Fork the [Meadow.Foundation.Featherwings](https://github.com/WildernessLabs/Meadow.Foundation.Featherwings) repository and submit a pull request against the `develop` branch
+
+
+## Need Help?
+
+If you have questions or need assistance, please join the Wilderness Labs [community on Slack](http://slackinvite.wildernesslabs.co/).
